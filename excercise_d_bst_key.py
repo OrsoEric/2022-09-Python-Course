fbf0d5a9a35220fb0f74bd7cb77004cb0e3c6d68
Orso <30684972+OrsoEric@users.noreply.github.com>
2022-09-22 16:15:54 +0200

    almost finished the successor function

--- excercise_d_bst_key.py
+++ excercise_d_bst_key.py
@@ -6,6 +6,7 @@ class Node:
         """Construct a node with a Key and a value"""
         self._key = i_key
         self._value = i_value
+        self._father = None
         self._left = None
         self._right = None
         logging.debug(f"Construct Node | {self}")
@@ -19,6 +20,17 @@ class Node:
     def value( self ):
         """getter _value"""
         return self._value
+
+    @property
+    def father( self ):
+        """getter _father"""
+        return self._father
+
+    @father.setter
+    def father( self, i_father : "Node" ) -> bool:
+        """setter _father"""
+        self._father = i_father
+        return False
 
     @property
     def left( self ):
@@ -50,7 +62,6 @@ class Node:
         """Overload '<' between Node"""
         return self.key < rhs.key
 
-
     def __str__(self) -> str:
         """Stringfy. Show key, value and ID of three items"""
         return f"Key {self._key} | Value {self._value} | Self: {id(self)} | Left {id(self._left)} | Right {id(self._right)}"
@@ -60,7 +71,7 @@ class Binary_search_tree:
         """Empty tree constructor"""
         self._root = None
 
-    def add(self, i_key, i_value ) -> bool:
+    def add(self, i_key, i_value ) -> Node:
         """Add a new node to the tree"""
         #contruct a new node
         new_node = Node( i_key, i_value )
@@ -70,21 +81,25 @@ class Binary_search_tree:
             if (new_node == current_node):
                 #there can't be two nodes with same keys
                 logging.error(f"Trying to add node with same key {new_node.key} as existing node {current_node.key}")
-                return True
+                return None
             elif (new_node < current_node):
                 if (current_node.left is None):
                     current_node.left = new_node
+                    new_node.father = current_node
                 else:
                     current_node = current_node.left
             elif (new_node > current_node):
                 if (current_node.right is None):
                     current_node.right = new_node
+                    new_node.father = current_node
                 else:
                     current_node = current_node.right
             else:
                 raise Exception(f"Algorithmic error. Key {new_node.key} is neither < == nor > as  {current_node.key}")
         else:
             self._root = new_node
+            new_node.father = None
+        return new_node
 
     def find_node( self, i_key = None ):
         """Find a node with a given key"""
@@ -100,10 +115,43 @@ class Binary_search_tree:
             elif (i_key > current_node.key):
                 current_node = current_node.right
             else:
-                raise Exception(f"Algorithmic error. Key {i_key} is neither < == nor > as  {current_node.key}")
+                raise Exception(f"Algorithmic error. Key {i_key} is neither '<' '==' nor '>' as  {current_node.key}")
         else:
             return None
-    
+
+    def successor( self, i_node : Node = None):
+        """Find the smallest node that is bigger than the given node"""
+
+        if (i_node is None):
+            return None
+        #if I have a > child, go to major, and go as left as possible
+        if (i_node.right is not None):
+            i_node = i_node.right
+            while (i_node.left is not None):
+                i_node = i_node.left
+            return i_node
+        #I don't have a > child. I need to go up, and go down if I'm not already a <
+        elif (i_node.father is not None):
+            #go up, remember key
+            i_key = i_node.key
+            i_node = i_node.father
+            #while I'm the left child of the father, I found the successor
+            while (i_key > i_node.key):
+                i_key = i_node.key
+                i_node = i_node.father
+            else:
+                return i_node
+            #I'm the right child. I need to go up until I find a father for which I'm the left child
+            #else:
+                
+
+
+
+                #raise Exception(f"Not Implemented Yet")
+        #I can't go up if I'm the root, I dont' have a successor
+        else:
+            return None
+
 
     def show( self, i_node : Node = None, is_indent : str = "", is_branch : str = None ):
         """Show the tree structure, key and content"""
@@ -116,8 +164,10 @@ class Binary_search_tree:
             current_node = self._root
         else:
             current_node = i_node
-
-        s_print = f"{current_node.key} | {current_node.value}"
+        if (current_node.father is None):
+            s_print = f"{current_node.key} | {current_node.value}"
+        else:
+            s_print = f"{current_node.key} ({current_node.father.key}) | {current_node.value} | "
         #print the node
         print( is_indent + is_branch + s_print )
         if (current_node.left is not None):
@@ -139,22 +189,51 @@ if __name__ == "__main__":
     logging.basicConfig( level=logging.DEBUG, format='[%(asctime)s] %(module)s:%(lineno)d %(levelname)s> %(message)s' )
 
     #my_node = Node()
-    my_node = Node( 42, "Erd Tree Root" )
+    #my_node = Node( 42, "Erd Tree Root" )
     #print("Key: ", my_node.key)
-
     my_tree = Binary_search_tree()
-    print( my_tree )
-    my_tree.add( 42, "Erd Tree Root" )
-    my_tree.add( 40, "Lurnia" )
-    my_tree.add( 999, "Elden Nord" )
-    my_tree.add( 44, "Caelid" )
-    my_tree.add( 43, "Sellen" )
-    my_tree.add( 45, "Luden" )
-    my_tree.add( 41, "Troy" )
-    my_tree.add( 999, "Elden Lady" )
-    
-    print( "--------------------------------------------------" )
+    if (False):
+        print( my_tree )
+        my_tree.add( 42, "Erd Tree Root" )
+        node_search = my_tree.add( 40, "Lurnia" )
+        my_tree.add( 999, "Elden Nord" )
+        my_tree.add( 44, "Caelid" )
+        my_tree.add( 43, "Sellen" )
+        my_tree.add( 45, "Luden" )
+        my_tree.add( 41, "Troy" )
+        my_tree.add( 999, "Elden Lady" )
+
+    my_tree.add( 2, "*" )
+    my_tree.add( 1, "*" )
+    my_tree.add( 33, "*" )
+    my_tree.add( 25, "*" )
+    my_tree.add( 0, "*" )
+    my_tree.add( 40, "*" )
+    my_tree.add( 11, "*" )
+    my_tree.add( 34, "*" )
+    my_tree.add( 7, "*" )
+    my_tree.add( 12, "*" )
+    my_tree.add( 36, "*" )
+    my_tree.add( 13, "*" )
     my_tree.show()
+
+    print( "--------------------------------------------------" )
+
+    if (True):
+        #right
+        print( "Successor: ", my_tree.successor( my_tree.find_node(2) ) )
+        print( "Successor: ", my_tree.successor( my_tree.find_node(11) ) )
+        print( "Successor: ", my_tree.successor( my_tree.find_node(12) ) )
+
+        #father
+        print( "Successor: ", my_tree.successor( my_tree.find_node(1) ) )
+        print( "Successor: ", my_tree.successor( my_tree.find_node(7) ) )
+
+        print( "Successor: ", my_tree.successor( my_tree.find_node(13) ) )
+
+
+    print( "--------------------------------------------------" )
+    
     if (False):
         print( "--------------------------------------------------" )
         print("Test find_node")
@@ -162,7 +241,7 @@ if __name__ == "__main__":
         print( my_tree.find_node( 40 ) )
         print( my_tree.find_node( 999 ) )
         print( my_tree.find_node( 404 ) )
-        
+
     if (False):
         print( "--------------------------------------------------" )
         print("Test contains")
